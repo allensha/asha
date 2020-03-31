9f1c913efd94de02755be3f8c2d2707bc6d24f34
allensha <62872415+allensha@users.noreply.github.com>
2020-03-31 01:49:15 -0400

    Update gctx.mli

--- gctx.mli
+++ gctx.mli
@@ -1,26 +1,5 @@
-(** The "Graphics Context" component of the GUI library. *)
-(** A graphics context represents a portion of the window to which widgets will
-    be drawn.
-
-    The drawing primitives in this module are all relative to the graphics
-    context. This means that when a widget needs to draw on the screen, it need
-    not know its absolute position. The graphics context is responsible for
-    translating the relative positions passed into the drawing routines into
-    absolute positions on the screen.
-
-    The graphics context also includes other information for basic drawing (such
-    as the current pen color.)
-
-    Note that this module defines a persistent (immutable) data structure. The
-    operations here use a given graphics context to create a new one with the
-    specified characteristics. They do not modify their arguments. *)
-
 (** The main (abstract) type of graphics contexts. *)
 type gctx
-
-(****************)
-(** {1 Colors } *)
-(****************)
 
 (** A type for colors, specified by red, green and blue values in 
     the range [0 .. 255] inclusive *) 		
@@ -58,21 +37,11 @@ val with_color : gctx -> color -> gctx
 (** Produce a new gctx with a different pen thickness *)
 val with_thickness : gctx -> int -> gctx
 
-	 
-(*****************)
-(** {1 Drawing } *)
-(*****************)
-
 (** A widget-relative position *)
 type position = int * int
 
 (** A width and height paired together. *)
 type dimension = int * int
-
-(** Various primitive drawing routines. Arguments are widget-local
-    coordinates. *)
-
-
 
 (** Draw a line between the two specified positions *)
 val draw_line : gctx -> position -> position -> unit
@@ -104,11 +73,6 @@ val text_size : string -> dimension
 val draw_point : gctx -> position -> unit
 
 val draw_points : gctx -> position list -> unit
-
-(************************)
-(** {1 Event Handling } *)
-(************************)
-
 
 type event_type =
   | KeyPress of char     (* User pressed the following key     *)
