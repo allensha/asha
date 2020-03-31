17b1024bd9604ac62fa2471db3c7efdeed5fdeb4
allensha <62872415+allensha@users.noreply.github.com>
2020-03-31 01:51:35 -0400

    Update paint.ml

--- paint.ml
+++ paint.ml
@@ -3,37 +3,14 @@
 ;; open Gctx
 ;; open Widget
 
-(******************************************)
-(**    SHAPES, MODES, and PROGRAM STATE   *)
-(******************************************)
-
 (** A location in the paint_canvas widget *)
 type point = position (* from Gctx *)
 
-(** The shapes that are visible in the paint canvas -- these make up the
-    picture that the user has drawn, as well as any other "visible" elements
-    that must show up in the canvas area (e.g. a "selection rectangle"). At
-    the start of the homework, the only available shape is a line.  *)
-(* TODO: You will modify this definition in Tasks 3, 4, 5 and maybe 6. *)
 type shape = 
   | Line of {color: color;  p1: point; p2: point; thickness: int}
   | Points of {color: Gctx.color; points: point list; thickness: int}
   | Ellipse of {color: Gctx.color;  p1: point; rx: int; ry: int; thickness: int}
 
-(** These are the possible interaction modes that the paint program might be
-    in. Some interactions require two modes. For example, the GUI might
-    recognize the first mouse click as starting a line and a second mouse
-    click as finishing the line.
-
-    To start out, there are only two modes:
-
-      - LineStartMode means the paint program is waiting for the user to make
-        the first click to start a line.
-
-      - LineEndMode means that the paint program is waiting for the user's
-        second click. The point associated with this mode stores the location of
-        the user's first mouse click.  *)
-(* TODO: You will need to modify this type in Tasks 2, 4, and maybe 6. *)
 type mode = 
   | LineStartMode
   | LineEndMode of point
@@ -52,9 +29,6 @@ type state = {
 
   (** The currently selected pen color. *)
   mutable color : color;
-
-  (* TODO: You will need to add new state for Tasks 2, 3, 5, and *)
-  (* possibly 6 *) 
   
   mutable preview: shape option;
   
@@ -66,7 +40,6 @@ let paint : state = {
   shapes = Deque.create ();
   mode = LineStartMode;
   color = black;
-  (* TODO: You will need to add new state for Tasks 2, 3, 5, and maybe 6 *)
   preview = None;
   thickness = 1;
 }
@@ -74,7 +47,6 @@ let paint : state = {
 (** This function creates a graphics context with the appropriate
     pen color.
 *)
-(* TODO: Your will need to modify this function in Task 5 *)
 let with_params (g: gctx) (c: color) (t: int): gctx =
   let g = with_color g c in
   let g_t = with_thickness g t in
@@ -84,17 +56,6 @@ let with_params (g: gctx) (c: color) (t: int): gctx =
 (**    MAIN CANVAS REPAINTING    *)
 (*********************************)
 
-(** The paint_canvas repaint function.
-
-    This function iterates through all the drawn shapes (in order of least
-    recent to most recent so that they are layered on top of one another
-    correctly) and uses the Gctx.draw_xyz functions to display them on the
-    canvas.  *)
-
-(* TODO: You will need to modify this repaint function in Tasks 2, 3,
-   4, and possibly 6. For example, if the user is performing some
-   operation that provides "preview" (see Task 2) the repaint function
-   must also show the preview.  *)
 let repaint (g: gctx) : unit =
   let draw_shape (s: shape) : unit =
     begin match s with
@@ -114,27 +75,14 @@ let repaint (g: gctx) : unit =
 let ((paint_canvas : widget), (paint_canvas_controller : notifier_controller)) =
   canvas (600, 350) repaint
 
-
-(************************************)
-(**  PAINT CANVAS EVENT HANDLER     *)
-(************************************)
-
 (** The paint_action function processes all events that occur 
     in the canvas region. *)
-(* TODO: Tasks 2, 3, 4, 5, and 6 involve changes to paint_action. *)
 let paint_action (gc:gctx) (event:event) : unit =
   let p  = event_pos event gc in  (* mouse position *)
   begin match (event_type event) with
     | MouseDown ->
-       (* This case occurs when the mouse has been clicked in the
-          canvas, but before the button has been released. How we
-          process the event depends on the current mode of the paint
-          canvas.  *)
       (begin match paint.mode with 
          | LineStartMode ->
-           (* The paint_canvas was waiting for the first click of a line,   
-              so change it to LineEndMode, recording the starting point of  
-              the line. *)
            paint.mode <- LineEndMode p
          | PointMode -> paint.preview <- Some (Points {color = paint.color; 
                                                points = [p];
@@ -144,9 +92,6 @@ let paint_action (gc:gctx) (event:event) : unit =
          | _ -> ()
           end)
     | MouseDrag ->
-      (* In this case, the mouse has been clicked, and it's being dragged    
-         with the button down. Initially there is nothing to do, but you'll  
-         need to update this part for Task 2, 3, 4 and maybe 6. *)
          (begin match paint.mode with 
          | LineStartMode ->
            paint.mode <- LineEndMode p
@@ -171,8 +116,6 @@ let paint_action (gc:gctx) (event:event) : unit =
                                thickness = paint.thickness})
        end)
     | MouseUp ->
-      (* In this case there was a mouse button release event. TODO: Tasks 2, *)
-      (* 3, 4, and possibly 6 need to do something different here.           *)
        (begin match paint.mode with 
          | LineEndMode p1 ->
            Deque.insert_tail (Line {color=paint.color; p1=p1; p2=p;
@@ -202,9 +145,6 @@ let paint_action (gc:gctx) (event:event) : unit =
           | _ -> ()
        end)
     | _ -> ()
-    (* This catches the MouseMove event (where the user moved the mouse over  
-       the canvas without pushing any buttons) and the KeyPress event (where 
-       the user typed a key when the mouse was over the canvas). *)
   end
 
 (** Add the paint_action function as a listener to the paint_canvas *)
@@ -215,28 +155,12 @@ let paint_action (gc:gctx) (event:event) : unit =
 (** TOOLBARS AND PAINT PROGRAM LAYOUT *)
 (**************************************)
 
-(** This part of the program creates the other widgets for the
-   paint program -- the buttons, color selectors, etc., and
-   lays them out in the top - level window. *)
-(* TODO: Tasks 1, 2, 4, 5, and 6 involving adding new buttons or
-   changing the layout of the Paint GUI. Initially the layout is very
-   ugly because we use only the hpair widget demonstrated in
-   Lecture. Task 1 is to make improvements to make the layout more
-   appealing. You may choose to arrange the buttons and other GUI
-   elements of the paint program however you like (so long as it is
-   easily apparent how to use the interface ).  The sample screen shot
-   of our solution provides one possible design.  Also, feel free to
-   improve the visual components of the GUI, for example, our solution
-   puts borders around the buttons and uses a custom "color button"
-   that changes its appearance based on whether or not the color is
-   currently selected.  *)
-
 (** Create the Undo button *)
 let (w_undo, lc_undo, nc_undo) = button "Undo"
 
 (** This function runs when the Undo button is clicked.
    It simply removes the last shape from the shapes deque. *)
-(* TODO: You need to modify this in Task 2, 3 and 4. *)
+
 let undo () : unit =
   paint.preview <- None;
   (begin match paint.mode with 
@@ -294,19 +218,14 @@ let spacer : widget = space (10,10)
 
 
 (** The mode toolbar, initially containing just the Undo button. *)
-(*  TODO: you will need to add more buttons to the toolbar in 
-    Tasks 3, 5, and possibly 6. *)
+
 let mode_toolbar : widget = 
   let ws = [border w_point; spacer; border w_line; spacer; border w_ellipse; 
             spacer; border w_undo; spacer; switch_w] in
   hlist ws
 
 (* The color selection toolbar. *)
-(* This toolbar contains an indicator for the currently selected color 
-   and some buttons for changing it. Both the indicator and the buttons 
-   are small square widgets built from this higher-order function. *)
-(** Create a widget that displays itself as colored square with the given 
-    width and color specified by the [get_color] function. *)
+
 let colored_square (width:int) (get_color:unit -> color)
   : widget * notifier_controller =
   let repaint_square (gc:gctx) =
@@ -314,16 +233,10 @@ let colored_square (width:int) (get_color:unit -> color)
     fill_rect (with_color gc c) (0, 0) (width-1, width-1) in   
   canvas (width,width) repaint_square
 
-(** The color_indicator repaints itself with the currently selected 
-    color of the paint application. *)
 let color_indicator =
   let indicator,_ = colored_square 24 (fun () -> paint.color) in
   let lab, _ = label "Current Color" in
   border (hpair lab indicator)
-
-(** color_buttons repaint themselves with whatever color they were created 
-    with. They are also installed with a mouseclick listener
-    that changes the selected color of the paint app to their color. *)  
 
 let color_button (c: color) : widget =
   let w,nc = colored_square 10 (fun () -> c) in
@@ -345,16 +258,10 @@ let color_button (c: color) : widget =
 (** The top-level paint program widget: a combination of the
     mode_toolbar, the color_toolbar and the paint_canvas widgets.
 *)
-(* TODO: Task 1 (and others) involve modifing this layout to add new
-   buttons and make the layout more aesthetically appealing. *)
+
 let paint_widget =
    let ws = [paint_canvas; mode_toolbar; spacer; slider_w; spacer; color_toolbar] in
    vlist ws
-
-
-(**************************************)
-(**      Start the application        *)
-(**************************************)
 
 (** Run the event loop to process user events. *)
 ;; Eventloop.run paint_widget
